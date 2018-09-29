7978981bfc3b593bcd14dd16e678984c50a5cfc6
Nicolás Andrés Gallinal <ngallinal@hexacta.com>
2018-09-29 18:14:56 -0300

    generate definition file, removed unnecesary dependencies, clean after build

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,14 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonJS from 'rollup-plugin-commonjs';
-
 const external = [ 'assert', 'vm-browserify' ]
 
 export default [{
-  input: 'dist/es5/index.js',
+  input: 'lib/es5/index.js',
   output: {
     name: 'matecocido',
-    file: 'dist/matecocido.js',
+    file: 'lib/matecocido.js',
     format: 'cjs'
   },
   context: 'window',
   external,
-  plugins: [
-    resolve({
-      preferBuiltins: false,
-      browser: true
-    }),
-    commonJS({
-      // include: 'node_modules/**',
-      namedExports: {
-        'assert': [ '*' ],
-        'vm-browserify': ['runInNewContext']
-      }
-    })
-  ],
   onwarn(warning, warn) {
     // vm-browserify uses eval in a sandboxed iframe
     // rollup has no way to know.
@@ -32,9 +16,9 @@ export default [{
     warn(warning);
   }
 },{
-  input: 'dist/es6/index.js',
+  input: 'lib/es6/index.js',
   output: {
-    file: 'dist/matecocido.esm.js',
+    file: 'lib/matecocido.esm.js',
     format: 'esm'
   },
   external
